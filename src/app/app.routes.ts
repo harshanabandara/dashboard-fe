92c9964a854de8ef8c2659d94c4950bf4c6c35d4
Harshana <e16049@eng.pdn.ac.lk>
2025-01-05 22:47:47 +0530

    initial project setup with routing enabled

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'users', component: UserManagementComponent},
+    
+    {path: '', redirectTo: '/login',pathMatch: 'full'}
+
+];
