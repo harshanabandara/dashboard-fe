d97d4c7f52ec06dd9d3cd850dcffac3a3be573a5
Harshana <e16049@eng.pdn.ac.lk>
2025-01-13 17:49:49 +0530

    integrated dashboard endpoint.

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { baseURL } from '../../constants';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,15 @@ import { inject, Injectable } from '@angular/core';
 export class DataService {
 
   http = inject(HttpClient);
+  private url = `${baseURL}/api/dashboard`
   constructor() { }
+
+  getDashboard() {
+    return this.http.get(this.url).pipe(
+      tap((response) => {
+        console.log(response)
+      })
+    )
+  }
 
 }
