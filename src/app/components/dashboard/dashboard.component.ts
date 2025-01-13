03962f4e1a584ea9c339d7d698b18ce02d307011
Harshana <e16049@eng.pdn.ac.lk>
2025-01-13 17:50:37 +0530

    added the dashboard.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { GetSummary } from '../../core/store/summary/summary.actions';
+import { Observable, tap } from 'rxjs';
+import { SummaryState } from '../../core/store/summary/summary.state';
+import { UsersGraphComponent } from "../users-graph/users-graph.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [UsersGraphComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  totalUsers: number = 10;
-  totalActiveUsers: number = 2;
-  lastUserAdded: string = "Harshana Bandara"
+export class DashboardComponent implements OnInit, OnDestroy {
+
+  private store = inject(Store);
+
+  @Select(SummaryState.getState) summary$: Observable<any> | undefined;
+
+  summarySignal = this.store.selectSignal(SummaryState.getState)
+
+  ngOnInit(): void {
+    this.store.dispatch(new GetSummary()).pipe(
+      tap((response) => {
+        console.log(response)
+      })
+    )
+  }
+
+  ngOnDestroy() {
+  }
+  formatDate(date: string): string {
+    if (date.length == 0) {
+      return '';
+    }
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(new Date(date));
+  }
+
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from "../dashboard/dashboard.component";
+import { UserManagementComponent } from "../user-management/user-management.component";
 
 @Component({
   selector: 'app-home',
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, UserManagementComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
